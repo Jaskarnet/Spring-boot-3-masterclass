d0d8047f7d6326509405e7c496bd96ac291e2115
Jan Kucharski <jankucharski99@gmail.com>
2025-02-04 06:28:08 +0100

    Add bidirectional mapping for InstructorDetail
    - Updated `InstructorDetail` entity with a bidirectional `@OneToOne` mapping to `Instructor`.
    - Added `mappedBy = "instructorDetail"` to establish the relationship.
    - Included cascade types (DETACH, MERGE, PERSIST, REFRESH).
    
    Use Cases:
    1. findInstructorDetail(appDAO)
       - Retrieves an `InstructorDetail` by its ID and prints both the detail and its associated `Instructor`.
       - Demonstrates bidirectional access from `InstructorDetail` to `Instructor`.
    
    2. deleteInstructorDetail(appDAO)
       - Deletes an `InstructorDetail` by its ID.
       - Ensures the associated `Instructor` is not removed.

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/net/jaskar/cruddemo/dao/AppDAO.java
@@ -0,0 +1,17 @@
+package net.jaskar.cruddemo.dao;
+
+import net.jaskar.cruddemo.entity.Instructor;
+import net.jaskar.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+}
